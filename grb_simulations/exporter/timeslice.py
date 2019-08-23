fd943892c1e9de03f42c4807d1bdd807bcc64813
Simone <simotrone@gmail.com>
2019-08-23 14:14:27 +0200

    class and static methods

--- grb_simulations/exporter/timeslice.py
+++ grb_simulations/exporter/timeslice.py
@@ -11,7 +11,7 @@ class TimeSliceExporter:
         self.hdul = fits.open(input_filename)
         self.verbosity = verbosity
         if self.verbosity > 0:
-            self.info()
+            self.hdul.info()
         if tmax is not None and tmax < 1:
             raise Exception('Need a non-zero positive tmax')
         self.tmax = tmax
@@ -19,7 +19,7 @@ class TimeSliceExporter:
         self.model_filename = model_template
         self.model_tree = None
         if self.model_filename:
-            self.model_tree = self.parse_xml_model()
+            self.model_tree = self.parse_xml(self.model_filename)
 
         self.savings_dir = '.'
         if savings_dir:
@@ -35,9 +35,6 @@ class TimeSliceExporter:
                 if self.verbosity > 1:
                     print("The data dir already exists", file=sys.stderr)
 
-    def info(self):
-        self.hdul.info()
-
     # times in secs
     # energies in GeV
     # spectra in ph/cm2/s/GeV
@@ -46,16 +43,18 @@ class TimeSliceExporter:
         times    = self.hdul['TIMES'].data
         energies = self.hdul['ENERGIES'].data
         spectra  = self.hdul['SPECTRA'].data
+        mev_energies = [ene[0]*1000 for ene in energies]
         done = []
         for i, tsec in enumerate(times):
             # writing energies/spectra tsv
+            mev_spectra = [f/1000 for f in spectra[i]]
             time_slice_filename = os.path.join(self.savings_dir, "spec_{0:02d}.tsv".format(i))
-            self.write_slice_tsv(time_slice_filename, energies, spectra[i])
+            self.write_slice_tsv(time_slice_filename, mev_energies, mev_spectra)
             # writing model xml if template was provided
             xml_slice_filename = None
             if self.model_filename:
                 xml_slice_filename = os.path.join(self.savings_dir, self.model_filename.replace('.', '_{0:02d}.'.format(i)))
-                self.write_linked_model(os.path.basename(time_slice_filename), xml_slice_filename)
+                self.write_linked_model(self.model_tree, os.path.basename(time_slice_filename), xml_slice_filename)
             done.append((i, tsec[0], time_slice_filename, xml_slice_filename))
             if self.verbosity > 1:
                 print('slice {0:2d} {1:15f} sec > {2}{3}'.format(i, tsec[0], time_slice_filename, ', '+xml_slice_filename if xml_slice_filename else ''), file=sys.stderr)
@@ -64,30 +63,31 @@ class TimeSliceExporter:
                 break
         return done
 
-    def write_slice_tsv(self, output_filename, energies, spectra):
+    @staticmethod
+    def write_slice_tsv(output_filename, energies, spectra):
         if not len(energies) == len(spectra):
             raise Exception('Need the same number of elements between energies and spectra')
         with open(output_filename, mode='w', newline="\n") as fh:
             writer = csv.writer(fh, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for i, ene in enumerate(energies):
-                writer.writerow([ene[0], spectra[i]])
+                writer.writerow([ene, spectra[i]])
 
     # read the xml just once
-    def parse_xml_model(self):
-        if not self.model_filename:
-            raise Exception('Need a model file template')
-        tree = parse(self.model_filename)
+    @staticmethod
+    def parse_xml(filename):
+        tree = parse(filename)
         return tree
 
-    def write_linked_model(self, ref_file, output_xml_fn):
-        if not self.model_tree:
+    @classmethod
+    def write_linked_model(cls, model_tree, ref_file, output_xml_fn):
+        if not model_tree:
             raise Exception('Need a defined xml model tree')
-        source_library = self.model_tree.getroot()
+        source_library = model_tree.getroot()
         for source in source_library:
             spectrum = source.find('spectrum')
             if spectrum.attrib.get('type') != 'FileFunction':
                 continue
             spectrum.set('file', ref_file)
             break # we can stop at first occurrency
-        self.model_tree.write(output_xml_fn, xml_declaration=True, encoding="UTF-8")
+        model_tree.write(output_xml_fn, xml_declaration=True, encoding="UTF-8")
 
