dcba6f4bf44c53846ee1d549c57792c83fe68069
Simone <simotrone@gmail.com>
2019-08-30 14:17:49 +0200

    fix the starter script

--- grb_simulations/starter.sh
+++ grb_simulations/starter.sh
@@ -1,7 +1,10 @@
-python explore_fits.py run0406_ID000126.fits --template-model run0406_ID000126.xml --tmax 1800 --dec-shift 0.5 --dir dec_0.5 --model source_model.xml --save &
-python explore_fits.py run0406_ID000126.fits --template-model run0406_ID000126.xml --tmax 1800 --dec-shift 1.0 --dir dec_1.0 --model source_model.xml --save &
+DIR="data"
+TS="$DIR/timeslices.tsv"
+python export_run.py run0406_ID000126.fits run0406_ID000126.xml --dir $DIR --save $TS --verbose
+python run_simulations.py $TS --dir dec_0.5 --tmax 1800 --dec-shift 0.5 --model source_model.xml --save &
+python run_simulations.py $TS --dir dec_1.0 --tmax 1800 --dec-shift 1.0 --model source_model.xml --save &
 # ra +0.5 doesn't provide an on/off analysis
-#python explore_fits.py run0406_ID000126.fits --template-model run0406_ID000126.xml --tmax 1800 --ra-shift 0.5  --dir ra_0.5  --model source_model.xml --save &
-#python explore_fits.py run0406_ID000126.fits --template-model run0406_ID000126.xml --tmax 1800 --ra-shift 0.6  --dir ra_0.6  --model source_model.xml --save &
-python explore_fits.py run0406_ID000126.fits --template-model run0406_ID000126.xml --tmax 1800 --ra-shift 1.0 --dec-shift 1.0 --dir ra_1.0_dec_1.0 --model source_model.xml --save &
-python explore_fits.py run0406_ID000126.fits --template-model run0406_ID000126.xml --tmax 1800 --ra-shift 2.0 --dec-shift 2.0 --dir ra_2.0_dec_2.0 --model source_model.xml --save &
+python run_simulations.py $TS --dir ra_0.5 --tmax 1800 --ra-shift 0.5 --model source_model.xml --save &
+#python run_simulations.py $TS --dir ra_0.6 --tmax 1800 --ra-shift 0.6 --model source_model.xml --save &
+python run_simulations.py $TS --dir ra_1.0_dec_1.0 --tmax 1800 --ra-shift 1.0 --dec-shift 1.0 --model source_model.xml --save &
+python run_simulations.py $TS --dir ra_2.0_dec_2.0 --tmax 1800 --ra-shift 2.0 --dec-shift 2.0 --model source_model.xml --save &
