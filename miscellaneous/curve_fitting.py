c9836745ee20d28db8c95f8357b324129ad0a139
Simone <simotrone@gmail.com>
2019-09-05 17:20:35 +0200

    show fitting params

--- miscellaneous/curve_fitting.py
+++ miscellaneous/curve_fitting.py
@@ -3,48 +3,68 @@ import numpy as np
 import seaborn as sns
 from scipy import stats
 from scipy.stats import norm
-from scipy.stats import chi2
+from scipy.stats import chisquare
 from scipy.optimize import curve_fit
 
 # Example from:
 #    https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.random.normal.html#numpy.random.normal
 
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.chi2.html#scipy.stats.chi2
-def show_chi2_things():
-    df = 55
-    mean, var, skew, kurt = chi2.stats(df, moments="mvsk")
-    print("""==== Chi2 ====
-         mean: {}
-          var: {}
-    skewnewss: {}
-     kurtosis: {}""".format(mean, var, skew, kurt))
-
-    fig, ax = plt.subplots(1, 1)
-    # display the probabily density function (pdf):
-    x = np.linspace(chi2.ppf(0.01, df), chi2.ppf(0.99, df), 100)
-    ax.plot(x, chi2.pdf(x, df), 'r-', lw=5, alpha=0.6, label='chi2 pdf')
-
-    # freeze the distribution and display the frozen pdf:
-    rv = chi2(df)
-    ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
-
-    # check accuracy of cdf and ppf
-    vals = chi2.ppf([0.001, 0.5, 0.999], df)
-    print("Chi2 test:", np.allclose([0.001, 0.5, 0.999], chi2.cdf(vals, df)))
-
-    r = chi2.rvs(df, size=5000) # random variates
-    ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
-    ax.legend(loc='best', frameon=False)
-    plt.show()
-
 # normalized normal distribution
 def gauss(x, *p):
     A, mu, sigma = p
-    return A*np.exp(-1*(x-mu)**2/(2.*sigma**2))
+    exp_num = -1*(x-mu)**2
+    exp_den = 2.*sigma**2
+    return A * np.exp(exp_num / exp_den)
 
-# def gauss(x, *p):
-#     useless, mu, sigma = p
-#     return 1 / (sigma * np.sqrt(2*np.pi)) * np.exp(-1*(x-mu)**2/(2.*sigma**2))
+def gauss2(x, *p):
+    mu, sigma = p
+    norm_factor = 1 / np.sqrt(2.* np.pi * sigma**2) 
+    exp_num = -1*(x-mu)**2
+    exp_den = 2.*sigma**2
+    return norm_factor * np.exp(exp_num / exp_den )
+
+# density plot is a smoothed continuous version of a histogram estimated from data.
+# common form of estimation is kernel density estimation
+def seaborn_distplot(x, my_bins=10, name=None):
+    hist, bin_edges = np.histogram(x, density=True, bins=my_bins)
+    # center the bins
+    bin_centres = (bin_edges[:-1] + bin_edges[1:])/2
+    initial_params = [1., 0., 1.]
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html#scipy.optimize.curve_fit
+    coeff, var_matrix, infodict, errmsg, ier = curve_fit(gauss, bin_centres, hist, p0=initial_params, full_output=True)
+    # print("infodict:",infodict, "\nerrmsg:", errmsg, "\nier:", ier)
+    perr = np.sqrt(np.diag(var_matrix)) # Standard deviation errors
+
+    print("Curve fit params:")
+    print("{0:>10s}  {1:9s}  {2:9s}".format("param no.", "value", "error"))
+    for i, c in enumerate(coeff):
+        print("{0:10d}  {1:+8.6f}  {2:+8.6f}".format(i, c, perr[i]))
+    hist_fit = gauss(bin_centres, *coeff)
+    my_ddof=my_bins-1-len(coeff)
+    chi2_ts, chi2_pvalue = chisquare(hist, f_exp=hist_fit, ddof=my_ddof)
+    print("""
+        Χ² test: {}
+         pvalue: {}
+           ddof: {}""".format(chi2_ts, chi2_pvalue, my_ddof))
+    # 
+    ss_res = np.sum((hist - hist_fit)**2)
+    ss_tot = np.sum((hist - np.mean(hist))**2)
+    r2 = 1 - (ss_res / ss_tot)
+    print("R2:", r2)
+
+    # Plot the distribution with a histogram and maximum likelihood gaussian distribution fit:
+    # ax = sns.distplot(x, fit=norm, kde=False)
+    ax = sns.distplot(x, bins=my_bins, fit_kws={'color': 'orange', 'label':'scipy.norm.pdf', 'linestyle':'--'}, kde=False, hist=True, fit=norm, norm_hist=False, label='data')
+    # ax.plot(bin_centres, hist, label='Test data', color='blue')
+    ax.plot(bin_centres, hist_fit, label='curve fit', linestyle="-.", color="green")
+
+
+    my_title = 'Seaborn'
+    if name:
+        my_title = '{} data dist ({})'.format(name, my_title)
+    plt.gca().set(title=my_title, ylabel='Normalized freq')
+    plt.legend(loc='best')
+    plt.show()
 
 def matplot_all_together(x, my_bins=10):
     hist, bin_edges = np.histogram(x, density=True, bins=my_bins)
@@ -64,32 +84,6 @@ def matplot_all_together(x, my_bins=10):
     plt.legend(loc='best')
     plt.show()
 
-# density plot is a smoothed continuous version of a histogram estimated from data.
-# common form of estimation is kernel density estimation
-def seaborn_distplot(x, my_bins=10, name=None):
-    hist, bin_edges = np.histogram(x, density=True, bins=my_bins)
-    # center the bins
-    bin_centres = (bin_edges[:-1] + bin_edges[1:])/2
-    initial_params = [1., 0., 1.]
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html#scipy.optimize.curve_fit
-    coeff, var_matrix = curve_fit(gauss, bin_centres, hist, p0=initial_params)
-    print('Curve fit params (A, mu, sigma):', coeff)
-    print('Standard deviation errors:', np.sqrt(np.diag(var_matrix)))
-    hist_fit = gauss(bin_centres, *coeff)
-
-    # Plot the distribution with a histogram and maximum likelihood gaussian distribution fit:
-    # ax = sns.distplot(x, fit=norm, kde=False)
-    ax = sns.distplot(x, bins=my_bins, fit_kws={'color': 'orange', 'label':'scipy.norm.pdf', 'linestyle':'--'}, kde=False, hist=True, fit=norm, norm_hist=False, label='data')
-    # ax.plot(bin_centres, hist, label='Test data', color='blue')
-    ax.plot(bin_centres, hist_fit, label='curve fit', linestyle="-.", color="green")
-
-    my_title = 'Seaborn'
-    if name:
-        my_title = '{} data dist ({})'.format(name, my_title)
-    plt.gca().set(title=my_title, ylabel='Normalized freq')
-    plt.legend(loc='best')
-    plt.show()
-
 def data_stats(x):
     nobs, (smin, smax), smean, svar, sskewness, skurtosis = stats.describe(x)
     # Delta Degrees of Freedom (divisor is N - ddof)
@@ -102,7 +96,7 @@ def data_stats(x):
        min / max: {} / {}
             mean: {}
              var: {}
-           sigma: {} (ddof=1)
+           stdev: {} (ddof=1)
        skewnewss: {}
         kurtosis: {}""".format(nobs, smin, smax, smean, svar, np.sqrt(svar), sskewness, skurtosis))
     print("Normal fit from data:", norm.fit(x))
@@ -113,7 +107,7 @@ if __name__ == '__main__':
     arr = [
         { 'name': 'Normal',   'data': np.random.normal(size = n_samples)   },
         { 'name': 'Rayleigh', 'data': np.random.rayleigh(size = n_samples) },
-        { 'name': 'Laplace',  'data': np.random.laplace(size = n_samples) },
+        # { 'name': 'Laplace',  'data': np.random.laplace(size = n_samples) },
         # { 'name': 'Chisquare', 'data': np.random.chisquare(57, size = n_samples) },
     ]
     for d in arr:
