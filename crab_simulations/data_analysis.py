0e3dc8a5e7a93f3b88de4619d9600f42632630f6
Simone <simotrone@gmail.com>
2019-09-12 15:38:06 +0200

    curve fitting

--- crab_simulations/data_analysis.py
+++ crab_simulations/data_analysis.py
@@ -4,7 +4,12 @@ import sys
 import statistics
 from lib.exporter.csv import CSVExporter as csvex
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy import stats
+from scipy.optimize import curve_fit
+import numpy as np
+
+# Example:
+# PYTHONPATH=../path/to/lib/ python data_analysis.py *tsv
 
 def data_summary_is_ok(data):
     if len(data) != 5*7:
@@ -111,6 +116,25 @@ def print_data_summary(data):
             li_ma_m["mean"], li_ma_m["stdev"],
             alpha_m["mean"]))
 
+def fitting_data(curve_fn, initial_params=[], x=[], y=[], verbosity=False, name=None):
+    res = curve_fit(curve_fn, x, y, p0=initial_params, full_output=verbosity)
+    coeff, var_matrix = res[:2]
+    if (len(res) > 2):
+        infodict, errmsg, ier = res[2:]
+        print("infodict: {}\nerrmsg: {}\nier: {}".format(infodict, errmsg, ier))
+    perr = np.sqrt(np.diag(var_matrix))
+    print("Curve fit params: {}".format(name))
+    print("{0:>10s}  {1:9s}  {2:9s}".format("param no.", "value", "error"))
+    for i, c in enumerate(coeff):
+        print("{0:10d}  {1:+8.6e}  {2:+8.6e}".format(i, c, perr[i]))
+    return coeff, perr
+
+def gauss(x, *params):
+    A, mu, sigma, Q = params
+    exp_num = -1 * (x-mu)**2
+    exp_den = 2. * sigma**2
+    return A * 1. / (2. * np.pi * sigma**2)* np.exp(exp_num / exp_den) + Q
+
 def dynamic_bin_number(arr, max_val=None):
     n = max(arr)-min(arr)
     if max_val is not None and n > max_val:
@@ -122,34 +146,35 @@ def dynamic_bin_number(arr, max_val=None):
 #   print(array_stats(d["ts_array"]))
 #   print(norm.fit(d["ts_array"]))
 #   sns.distplot(d["ts_array"], bins=50, kde=True, fit=norm, norm_hist=False)# , norm_hist=True) #array, bins=n_bins, fit=norm, norm_hist=True
-def create_hist(ax, data_arr, xlabel=None, color="blue", dyn_bins=False, exp_fmt=False, density_flag=True):
+def create_hist(ax, data_arr, xlabel=None, color="blue", dyn_bins=False, density_flag=True):
     n_bins = 50
     if dyn_bins:
         n_bins = dynamic_bin_number(data_arr)
-    stats = array_stats(data_arr)
+    sts = array_stats(data_arr)
+
+    counts_hist, bins_edges, bin_index_not_used = stats.binned_statistic(data_arr, data_arr, statistic='count', bins=n_bins)
+    bins_width = np.array(np.diff(bins_edges), float)
+    bins_centres = (bins_edges[:-1] + bins_edges[1:])/2
+
+    counts_hist_normalized = counts_hist / bins_width / np.sum(counts_hist)
+
+    fit_params_norm, pvalue_err_norm = fitting_data(gauss, initial_params=[1., sts['mean'], sts['stdev'], 0.], x=bins_centres, y=counts_hist_normalized, verbosity=False, name=xlabel)
+    fitted_hist_normalized = gauss(bins_centres, *fit_params_norm)
 
     if density_flag:
-        counts, bins, patches = ax.hist(data_arr, bins=n_bins, alpha=0.5, edgecolor=color, color=color, density=True)
-        ax.plot(bins, norm.pdf(bins, stats["mean"], stats["stdev"]), color="grey", linestyle="--", alpha=0.9)
+        ax.bar(bins_centres, height=counts_hist_normalized, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
+        ax.plot(bins_centres, stats.norm.pdf(bins_centres, sts["mean"], sts["stdev"]), color="orange", linestyle="--", alpha=0.9, label='stats.norm\nμ:{0:.2e}\nσ:{1:.2e}'.format(sts['mean'], sts['stdev']))
+        ax.plot(bins_centres, fitted_hist_normalized, label='curve fit\nμ:{0:.2e}\nσ:{1:.2e}'.format(fit_params_norm[1], fit_params_norm[2]), linestyle="-.", color="green")
+        ax.legend(loc='best')
     else:
+        # ax.bar(bins_centres, height=counts_hist, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
         counts, bins, patches = ax.hist(data_arr, bins=n_bins, alpha=0.5, edgecolor=color, color=color)
 
-    ax.axvline(stats["mean"],   color="grey", linestyle="--", alpha=0.9)
-    ax.axvline(stats["median"], color="black",  linestyle="-.", alpha=0.5)
+    ax.axvline(sts["mean"], color="orange", linestyle="--", alpha=0.9)
+    # ax.axvline(sts["median"], color="black",  linestyle="-.", alpha=0.5)
 
-    legend_fmt = 'bins=%d\nmean=%.2f\nmedian=%.2f\nσ=%.2f'
-    if exp_fmt:
-        legend_fmt = 'bins=%d\nmean=%.2e\nmedian=%.2e\nσ=%.2e'
-    ax.text(x=0.65, y=0.70, s=legend_fmt % (n_bins, stats["mean"], stats["median"], stats["stdev"]), transform=ax.transAxes)
-
-    # if int is True:
-    #     from matplotlib.ticker import FormatStrFormatter
-    #     bin_centers = [ (bins[i]+bins[i+1])*0.5 for i in range(len(bins)-1) ]
-    #     ax.set_xticks(bin_centers)
-    #     #ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
-    #     ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
     if xlabel is not None:
-        ax.set_xlabel(xlabel)
+        ax.set_xlabel('{} (bins: {})'.format(xlabel, n_bins))
 
 def plot_data_summary(data):
     rows_num=3
@@ -160,8 +185,8 @@ def plot_data_summary(data):
 
         create_hist(ax[0][0], d["ts_array"],    color="blue",   xlabel="TS")
         create_hist(ax[1][0], d["li_ma_array"], color="cyan",   xlabel="Li&Ma")
-        create_hist(ax[2][0], d["flux_array"],  color="green",  xlabel="Flux [ph/cm²/s]", exp_fmt=True)
-        create_hist(ax[0][1], d["N_on_array"],  color="orange", xlabel="N on",     dyn_bins=True)
+        create_hist(ax[2][0], d["flux_array"],  color="green",  xlabel="Flux [ph/cm²/s]" )
+        create_hist(ax[0][1], d["N_on_array"],  color="magenta", xlabel="N on",     dyn_bins=True)
         create_hist(ax[1][1], d["N_off_array"], color="red",    xlabel="N off",    dyn_bins=True)
         create_hist(ax[2][1], d["N_s_array"],   color="yellow", xlabel="N signal", dyn_bins=True)
 
@@ -200,7 +225,4 @@ if __name__ == '__main__':
 
     print_data_summary(ds.values())
     plot_data_summary(list(ds.values()))
-
-
-
 
