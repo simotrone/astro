d4fb304736dc6cae144642268fed3fea813548b5
Simone <simotrone@gmail.com>
2019-09-13 12:25:59 +0200

    cosmetics

--- crab_simulations/data_analysis.py
+++ crab_simulations/data_analysis.py
@@ -66,6 +66,58 @@ def data_summary(all_data_info):
             print("{0:15s} ({1:.0f} on, {2:2.0f} off, {3:3d} seed, {4:4d} tmax): Cannot calculate Li&Ma".format(i["name"], float(i["on_count"]), float(i["off_count"]), int(i["seed"]), int(i["tmax"])), file=sys.stderr)
     return o
 
+# WARNING: this function augment the input data struct
+def data_augmentation(data, bins_number=50):
+    fields = [
+        { 'name': 'ts',    'dyn_bins': False },
+        { 'name': 'li_ma', 'dyn_bins': False },
+        { 'name': 'flux',  'dyn_bins': False },
+        # { 'name': 'eflux', 'dyn_bins': False },
+        { 'name': 'N_on',  'dyn_bins': True },
+        { 'name': 'N_off', 'dyn_bins': True },
+        { 'name': 'N_s',   'dyn_bins': True },
+    ]
+    for data_name, d in data.items():
+        print(data_name, file=sys.stderr)
+        if 'hist' not in d:
+            d['hist'] = {}
+        if 'stats' not in d:
+            d['stats'] = {}
+
+        for f in fields:
+            f_name = f['name']
+            data_arr_ref = d['{}_array'.format(f_name)]
+            n_bins = dynamic_bin_number(data_arr_ref) if f['dyn_bins'] else bins_number
+
+            counts_hist, bins_edges, bin_index_not_used = stats.binned_statistic(data_arr_ref, data_arr_ref, statistic='count', bins=n_bins)
+            bins_width = np.array(np.diff(bins_edges), float)
+            bins_centres = (bins_edges[:-1] + bins_edges[1:])/2
+            counts_hist_normalized = counts_hist / bins_width / np.sum(counts_hist)
+
+            data_stats = array_stats(data_arr_ref)
+            d['stats'][f_name] = {
+                'n': data_stats['n'],
+                'mean': data_stats['mean'],
+                'stdev': data_stats['stdev'],
+                'median': data_stats['median'],
+            }
+
+            starting_parameters = [1., data_stats['mean'], data_stats['stdev']] #, 0.] # A, mu, sigma, Q
+            fit_coeff_norm, pvalue_err_norm = fitting_data(gauss, initial_params=starting_parameters, x=bins_centres, y=counts_hist_normalized, verbosity=False, name=data_name)
+
+            d['hist'][f_name] = {
+                'n_bins': n_bins,
+                'bins_edges': bins_edges,
+                'bins_centres': bins_centres,
+                'bins_width': bins_width,
+                'counts': counts_hist,
+
+                'counts_norm': counts_hist_normalized,
+                'fit_coeff_norm': fit_coeff_norm,
+                'pvalue_err_norm': pvalue_err_norm,
+            }
+    return data
+
 def array_stats(arr):
     stat = {
         "n": len(arr),
@@ -130,6 +182,19 @@ def fitting_data(curve_fn, initial_params=[], x=[], y=[], verbosity=False, name=
     return coeff, perr
 
 def gauss(x, *params):
+    A, mu, sigma = params
+    exp_num = -1 * (x-mu)**2
+    exp_den = 2. * sigma**2
+    return A * 1. / (2. * np.pi * sigma**2)* np.exp(exp_num / exp_den)
+
+def gauss0(x, *params):
+    A, mu, sigma = params
+    exp_num = -1 * (x-mu)**2
+    exp_den = 2. * sigma**2
+    return A * 1. / (2. * np.pi * sigma**2)* np.exp(exp_num / exp_den)
+
+# no good with the quote
+def gauss2(x, *params):
     A, mu, sigma, Q = params
     exp_num = -1 * (x-mu)**2
     exp_den = 2. * sigma**2
@@ -146,35 +211,27 @@ def dynamic_bin_number(arr, max_val=None):
 #   print(array_stats(d["ts_array"]))
 #   print(norm.fit(d["ts_array"]))
 #   sns.distplot(d["ts_array"], bins=50, kde=True, fit=norm, norm_hist=False)# , norm_hist=True) #array, bins=n_bins, fit=norm, norm_hist=True
-def create_hist(ax, data_arr, xlabel=None, color="blue", dyn_bins=False, density_flag=True):
-    n_bins = 50
-    if dyn_bins:
-        n_bins = dynamic_bin_number(data_arr)
-    sts = array_stats(data_arr)
-
-    counts_hist, bins_edges, bin_index_not_used = stats.binned_statistic(data_arr, data_arr, statistic='count', bins=n_bins)
-    bins_width = np.array(np.diff(bins_edges), float)
-    bins_centres = (bins_edges[:-1] + bins_edges[1:])/2
-
-    counts_hist_normalized = counts_hist / bins_width / np.sum(counts_hist)
-
-    fit_params_norm, pvalue_err_norm = fitting_data(gauss, initial_params=[1., sts['mean'], sts['stdev'], 0.], x=bins_centres, y=counts_hist_normalized, verbosity=False, name=xlabel)
-    fitted_hist_normalized = gauss(bins_centres, *fit_params_norm)
+def create_hist(ax, data, data_stats, xlabel=None, color="blue", dyn_bins=False, density_flag=True):
+    bins_centres = data['bins_centres']
+    bins_width   = data['bins_width']
+    fit_params_norm = data['fit_coeff_norm']
+    fitted_hist_norm = gauss(bins_centres, *fit_params_norm)
+    counts_hist_norm = data['counts_norm']
 
     if density_flag:
-        ax.bar(bins_centres, height=counts_hist_normalized, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
-        ax.plot(bins_centres, stats.norm.pdf(bins_centres, sts["mean"], sts["stdev"]), color="orange", linestyle="--", alpha=0.9, label='stats.norm\nμ:{0:.2e}\nσ:{1:.2e}'.format(sts['mean'], sts['stdev']))
-        ax.plot(bins_centres, fitted_hist_normalized, label='curve fit\nμ:{0:.2e}\nσ:{1:.2e}'.format(fit_params_norm[1], fit_params_norm[2]), linestyle="-.", color="green")
+        ax.bar(bins_centres, height=counts_hist_norm, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
+        ax.plot(bins_centres, stats.norm.pdf(bins_centres, data_stats["mean"], data_stats["stdev"]), color="orange", linestyle="--", alpha=0.9, label='stats.norm\nμ:{0:.2e}\nσ:{1:.2e}'.format(data_stats['mean'], data_stats['stdev']))
+        ax.plot(bins_centres, fitted_hist_norm, label='curve fit\nμ:{0:.2e}\nσ:{1:.2e}'.format(fit_params_norm[1], fit_params_norm[2]), linestyle="-.", color="green")
         ax.legend(loc='best')
     else:
         # ax.bar(bins_centres, height=counts_hist, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
         counts, bins, patches = ax.hist(data_arr, bins=n_bins, alpha=0.5, edgecolor=color, color=color)
 
-    ax.axvline(sts["mean"], color="orange", linestyle="--", alpha=0.9)
+    ax.axvline(data_stats["mean"], color="orange", linestyle="--", alpha=0.9)
     # ax.axvline(sts["median"], color="black",  linestyle="-.", alpha=0.5)
 
     if xlabel is not None:
-        ax.set_xlabel('{} (bins: {})'.format(xlabel, n_bins))
+        ax.set_xlabel('{} (bins: {})'.format(xlabel, data['n_bins']))
 
 def plot_data_summary(data):
     rows_num=3
@@ -183,20 +240,20 @@ def plot_data_summary(data):
         fig, ax = plt.subplots(nrows=rows_num, ncols=2, figsize=(9, rows_num*3))
         fig.suptitle(d["name"]+" "+str(d["tmax"]), va="top", ha="center")
 
-        create_hist(ax[0][0], d["ts_array"],    color="blue",   xlabel="TS")
-        create_hist(ax[1][0], d["li_ma_array"], color="cyan",   xlabel="Li&Ma")
-        create_hist(ax[2][0], d["flux_array"],  color="green",  xlabel="Flux [ph/cm²/s]" )
-        create_hist(ax[0][1], d["N_on_array"],  color="magenta", xlabel="N on",     dyn_bins=True)
-        create_hist(ax[1][1], d["N_off_array"], color="red",    xlabel="N off",    dyn_bins=True)
-        create_hist(ax[2][1], d["N_s_array"],   color="yellow", xlabel="N signal", dyn_bins=True)
+        create_hist(ax[0][0], d['hist']['ts'], d['stats']['ts'], color="blue", xlabel="TS")
+        create_hist(ax[1][0], d['hist']['li_ma'], d['stats']['li_ma'], color="cyan", xlabel="Li&Ma")
+        create_hist(ax[2][0], d['hist']['flux'], d['stats']['flux'], color="green", xlabel="Flux [ph/cm²/s]" )
+        create_hist(ax[0][1], d['hist']['N_on'], d['stats']['N_on'], color="magenta", xlabel="N on", dyn_bins=True)
+        create_hist(ax[1][1], d['hist']['N_off'], d['stats']['N_off'], color="red", xlabel="N off", dyn_bins=True)
+        create_hist(ax[2][1], d['hist']['N_s'], d['stats']['N_s'], color="yellow", xlabel="N signal", dyn_bins=True)
 
         # Important: first tight_layout(), after adjust for the title
         fig.tight_layout()
         fig.subplots_adjust(top=0.95)
         # plt.show()
-        plt.savefig("%s_%04d.png" % (d["name"], d["tmax"]), format="png")
+        plt.savefig("{0}_{1:04d}.png".format(d["name"], d["tmax"]), format="png")
         plt.close()
-
+        print("saving {0}_{1:04d}.png".format(d["name"], d["tmax"]), file=sys.stderr)
     return None
 
 if __name__ == '__main__':
@@ -222,6 +279,9 @@ if __name__ == '__main__':
     ds = data_summary(original_data)
     if not data_summary_is_ok(ds):
         exit(1)
+
+    # inplace augment
+    data_augmentation(ds, bins_number=50)
 
     print_data_summary(ds.values())
     plot_data_summary(list(ds.values()))
