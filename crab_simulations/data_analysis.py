24f017572a5545e90a20ae9eaa74fa7953fbec15
Simone <simotrone@gmail.com>
2019-09-23 18:05:05 +0200

    plot model ts and flux(es)

--- crab_simulations/data_analysis.py
+++ crab_simulations/data_analysis.py
@@ -1,84 +1,89 @@
-import argparse
-import os
-import sys
-import statistics
 from lib.exporter.csv import CSVExporter as csvex
-import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
 from scipy import stats
 from scipy.optimize import curve_fit
+import argparse
+import logging
+import matplotlib.pyplot as plt
 import numpy as np
+import os
+import statistics
+import sys
 
 # Example:
 # PYTHONPATH=../path/to/lib/ python data_analysis.py *tsv
 
-def data_summary_is_ok(data):
-    if len(data) != 5*7:
-        print("Data summary length is {} and should be {} (pointings x time_slots)".format(len(data), 5*7), file=sys.stderr)
+def data_summary_is_ok(data, pointings=None, time_slots=None, different_seeds=None):
+    if len(data) != pointings * time_slots:
+        logging.warning("Data summary length is {} and should be {} (pointings x time_slots)".format(len(data), pointings*time_slots))
         return False
+    # check array data
     for k in data:
-        # check array data
         for sk in data[k]:
             if type(data[k][sk]) != type([]):
                 continue
-            if len(data[k][sk]) == 5000:
+            if len(data[k][sk]) == different_seeds:
                 continue
-            print("not enough data for '{}'".format(k), file=sys.stderr)
-            print("  key '{}' has {} values and should be {}".format(sk, len(data[k][sk]), 5000))
+            logging.warning("not enough data for '{}'".format(k))
+            logging.warning("  key '{}' has {} values and should be {}".format(sk, len(data[k][sk]), different_seeds))
             return False
     return True
 
 def data_summary(all_data_info):
     o = {}
     for i in all_data_info:
-        key = i["name"] +"_"+ str(i["tmax"])
+        key = i['name'] +'_'+ str(i['tmax'])
         if key not in o:
             o[key] = {
-                "name": i["name"],
-                "tmax": int(i["tmax"]),
-                "ra":   float(i["ra"]),
-                "dec":  float(i["dec"]),
-                "seed_array":  [],
-                "flux_array":  [],
-                "eflux_array": [],
-                "ts_array":    [],
-                "li_ma_array": [],
-                "N_on_array":  [],
-                "N_off_array": [],
-                "N_s_array":   [],
-                "alpha_array": [],
+                'name': i['name'],
+                'tmax': int(i['tmax']),
+                'ra':   float(i['ra'])  if 'ra'  in i else None,
+                'dec':  float(i['dec']) if 'dec' in i else None,
+                'data': {
+                    'seed':  [],
+                    'ts':    [],
+                    'index_value': [],
+                    'index_error': [],
+                    'prefactor_value': [],
+                    'prefactor_error': [],
+                    'pivot_value': [],
+                    'pivot_error': [],
+                    'flux':  [],
+                    'eflux': [],
+                    'N_on':  [],
+                    'N_off': [],
+                    'N_exc': [],
+                    'alpha': [],
+                    'li_ma': [],
+                }
             }
 
-        o[key]["seed_array"].append(int(i["seed"]))
-        o[key]["flux_array"].append(float(i["flux"]))
-        o[key]["eflux_array"].append(float(i["eflux"]))
-        o[key]["ts_array"].append(float(i["ts"]))
-        o[key]["li_ma_array"].append(float(i["li_ma"]) if i["li_ma"] is not None else 0)
-        o[key]["N_on_array"].append(float(i["on_count"]))
-        o[key]["N_off_array"].append(float(i["off_count"]))
-        o[key]["alpha_array"].append(float(i["alpha"]))
-
-        N_s = float(i['on_count']) - float(i['alpha']) * float(i['off_count'])
-        o[key]["N_s_array"].append(N_s)
+        o[key]['data']['seed'].append(int(i['seed']))
+        o[key]['data']['ts'].append(float(i['ts']))
+        o[key]['data']['flux'].append(float(i['flux']))
+        o[key]['data']['eflux'].append(float(i['eflux']))
+        o[key]['data']['N_on'].append(float(i['on_count']))
+        o[key]['data']['N_off'].append(float(i['off_count']))
+        o[key]['data']['N_exc'].append(float(i['excess_count']))
+        o[key]['data']['alpha'].append(float(i['alpha']))
+        o[key]['data']['li_ma'].append(float(i['li_ma']) if i['li_ma'] != '' else 0)
 
         if float(i["ts"]) < 0:
-            print("{0:15s} ({1:.0f} on, {2:2.0f} off, {3:3d} seed, {4:4d} tmax): Negative ts {5:.2f}".format(i["name"], float(i["on_count"]), float(i["off_count"]), int(i["seed"]), int(i["tmax"]), float(i["ts"])), file=sys.stderr)
+            logging.warning("{0:15s} ({1:.0f} on, {2:2.0f} off, {3:3d} seed, {4:4d} tmax): Negative ts {5:.2f}".format(i["name"], float(i["on_count"]), float(i["off_count"]), int(i["seed"]), int(i["tmax"]), float(i["ts"])))
         elif i["li_ma"] is None:
-            print("{0:15s} ({1:.0f} on, {2:2.0f} off, {3:3d} seed, {4:4d} tmax): Cannot calculate Li&Ma".format(i["name"], float(i["on_count"]), float(i["off_count"]), int(i["seed"]), int(i["tmax"])), file=sys.stderr)
+            logging.warning("{0:15s} ({1:.0f} on, {2:2.0f} off, {3:3d} seed, {4:4d} tmax): Cannot calculate Li&Ma".format(i["name"], float(i["on_count"]), float(i["off_count"]), int(i["seed"]), int(i["tmax"])))
     return o
 
 # WARNING: this function augment the input data struct
 def data_augmentation(data, bins_number=50):
     fields = [
         { 'name': 'ts',    'dyn_bins': False },
-        { 'name': 'li_ma', 'dyn_bins': False },
         { 'name': 'flux',  'dyn_bins': False },
-        # { 'name': 'eflux', 'dyn_bins': False },
-        { 'name': 'N_on',  'dyn_bins': True },
-        { 'name': 'N_off', 'dyn_bins': True },
-        { 'name': 'N_s',   'dyn_bins': True },
+        { 'name': 'eflux', 'dyn_bins': False },
     ]
     for data_name, d in data.items():
-        print(data_name, file=sys.stderr)
+        logging.warning(data_name)
         if 'hist' not in d:
             d['hist'] = {}
         if 'stats' not in d:
@@ -86,35 +91,33 @@ def data_augmentation(data, bins_number=50):
 
         for f in fields:
             f_name = f['name']
-            data_arr_ref = d['{}_array'.format(f_name)]
+            data_arr_ref = d['data'][f_name]
             n_bins = dynamic_bin_number(data_arr_ref) if f['dyn_bins'] else bins_number
 
+            # counts histogram
             counts_hist, bins_edges, bin_index_not_used = stats.binned_statistic(data_arr_ref, data_arr_ref, statistic='count', bins=n_bins)
             bins_width = np.array(np.diff(bins_edges), float)
             bins_centres = (bins_edges[:-1] + bins_edges[1:])/2
-            counts_hist_normalized = counts_hist / bins_width / np.sum(counts_hist)
+
+            # counts_hist_normalized = counts_hist / bins_width / np.sum(counts_hist)
 
             data_stats = array_stats(data_arr_ref)
-            d['stats'][f_name] = {
-                'n': data_stats['n'],
-                'mean': data_stats['mean'],
-                'stdev': data_stats['stdev'],
-                'median': data_stats['median'],
-            }
+            d['stats'][f_name] = data_stats
 
-            starting_parameters = [1., data_stats['mean'], data_stats['stdev']] #, 0.] # A, mu, sigma, Q
-            fit_coeff_norm, pvalue_err_norm = fitting_data(gauss, initial_params=starting_parameters, x=bins_centres, y=counts_hist_normalized, verbosity=False, name=data_name)
+            starting_parameters = [1., data_stats['mean'], data_stats['stdev']] # A, mu, sigma
+            fit_coeff, pvalue_err = fitting_data(gauss, initial_params=starting_parameters, x=bins_centres, y=counts_hist, verbosity=False, name=data_name)
 
             d['hist'][f_name] = {
                 'n_bins': n_bins,
+                'counts': counts_hist,
                 'bins_edges': bins_edges,
                 'bins_centres': bins_centres,
                 'bins_width': bins_width,
-                'counts': counts_hist,
-
-                'counts_norm': counts_hist_normalized,
-                'fit_coeff_norm': fit_coeff_norm,
-                'pvalue_err_norm': pvalue_err_norm,
+                'fit_coeff':  fit_coeff,
+                'pvalue_err': pvalue_err,
+                # 'counts_norm': counts_hist_normalized,
+                # 'fit_coeff_norm': fit_coeff_norm,
+                # 'pvalue_err_norm': pvalue_err_norm,
             }
     return data
 
@@ -129,20 +132,17 @@ def array_stats(arr):
 
 def print_data_summary(data):
     fields = [
-      #   h_format,      v_format,             title,             sub_t
-        [   "%15s",        "%15s",          "fs ref",      "==========", ],
-        [   "%10s",      "%10.4f",              "RA",              "==", ],
-        [   "%10s",      "%10.4f",             "Dec",             "===", ],
-        [    "%6s",         "%6d",            "tmax",            "====", ],
-        [    "%6s",         "%6d",           "seeds",           "=====", ],
-        [   "%18s", "%9.2e±%8.2e", "flux [ph/cm²/s]", "===============", ],
-        [   "%16s", "%9.2f±%6.2f",              "TS",              "==", ],
-      # [    "%6s",       "%6.2f",             "√TS",             "===", ],
-        [   "%15s", "%8.2f±%6.2f",            "N_on",            "====", ],
-        [   "%15s", "%8.2f±%6.2f",           "N_off",           "=====", ],
-        [   "%15s", "%8.2f±%6.2f",             "N_s",             "===", ],
-        [   "%11s", "%6.2f±%4.2f",           "Li&Ma",           "=====", ],
-        [    "%7s",       "%7.4f",           "alpha",           "=====", ],
+      #   h_format,      v_format,               title,             sub_t
+        [   "%15s",        "%15s",            "fs ref",      "==========", ],
+        [   "%10s",        "%10s",                "RA",              "==", ],
+        [   "%10s",        "%10s",               "Dec",             "===", ],
+        [    "%6s",         "%6d",              "tmax",            "====", ],
+        [    "%6s",         "%6d",             "seeds",           "=====", ],
+        [   "%16s", "%9.2f±%6.2f",                "TS",              "==", ],
+        [   "%18s", "%9.2e±%8.2e",   "flux [ph/cm²/s]", "===============", ],
+        [   "%18s", "%9.2e±%8.2e", "eflux [erg/cm²/s]", "===============", ],
+        [   '%26s', '%10.2f %7.2f %6.2f', 'TS fitting (A, μ, σ)', '=======', ],
+        [   '%23s', '%10.2f %5.2f %5.2f', 'TS pvalue (A, μ, σ)',  '=======', ],
     ]
 
     header_fmt = " ".join([r[0] for r in fields]) # headers format
@@ -150,23 +150,21 @@ def print_data_summary(data):
     print(header_fmt % tuple([r[2] for r in fields])) # titles
     print(header_fmt % tuple([r[3] for r in fields])) # sub_titles separator
     for d in sorted(data, key=lambda i: (-1*i["tmax"], i["ra"], i["dec"])):
-        n_seeds = len(d["seed_array"])
-        flux_m = array_stats(d["flux_array"])
-        ts_m   = array_stats(d["ts_array"])
-        N_on_m  = array_stats(d["N_on_array"])
-        N_off_m = array_stats(d["N_off_array"])
-        N_s_m   = array_stats(d["N_s_array"])
-        li_ma_m = array_stats(d["li_ma_array"])
-        alpha_m = array_stats(d["alpha_array"]) # useless
+        n_seeds = len(d['data']['seed'])
+        ts_m    = array_stats(d['data']['ts'])
+        flux_m  = array_stats(d['data']['flux'])
+        eflux_m = array_stats(d['data']['eflux'])
+        # print(d)
         print(values_fmt % (d["name"], d["ra"], d["dec"], d["tmax"], n_seeds,
-            flux_m["mean"], flux_m["stdev"],
             ts_m["mean"], ts_m["stdev"],
-            # sqrt_ts_m["mean"],
-            N_on_m["mean"],  N_on_m["stdev"],
-            N_off_m["mean"], N_off_m["stdev"],
-            N_s_m["mean"],   N_s_m["stdev"],
-            li_ma_m["mean"], li_ma_m["stdev"],
-            alpha_m["mean"]))
+            flux_m["mean"], flux_m["stdev"],
+            eflux_m["mean"], eflux_m["stdev"],
+            d['hist']['ts']['fit_coeff'][0],
+            d['hist']['ts']['fit_coeff'][1],
+            abs(d['hist']['ts']['fit_coeff'][2]),
+            d['hist']['ts']['pvalue_err'][0],
+            d['hist']['ts']['pvalue_err'][1],
+            d['hist']['ts']['pvalue_err'][2] ))
 
 def fitting_data(curve_fn, initial_params=[], x=[], y=[], verbosity=False, name=None):
     res = curve_fit(curve_fn, x, y, p0=initial_params, full_output=verbosity)
@@ -185,7 +183,7 @@ def gauss(x, *params):
     A, mu, sigma = params
     exp_num = -1 * (x-mu)**2
     exp_den = 2. * sigma**2
-    return A * 1. / (2. * np.pi * sigma**2)* np.exp(exp_num / exp_den)
+    return A * np.exp(exp_num / exp_den)
 
 def gauss0(x, *params):
     A, mu, sigma = params
@@ -200,10 +198,14 @@ def gauss2(x, *params):
     exp_den = 2. * sigma**2
     return A * 1. / (2. * np.pi * sigma**2)* np.exp(exp_num / exp_den) + Q
 
-def dynamic_bin_number(arr, max_val=None):
+def dynamic_bin_number(arr, max_val=None, min_val=None):
     n = max(arr)-min(arr)
     if max_val is not None and n > max_val:
         n = max_val
+    if min_val is not None and n < min_val:
+        n = min_val
+    if n < 1:
+        n = 2
     return int(n)
 
 # seaborn graph with distplot. Same data, same gaussian loc/scale
@@ -211,49 +213,53 @@ def dynamic_bin_number(arr, max_val=None):
 #   print(array_stats(d["ts_array"]))
 #   print(norm.fit(d["ts_array"]))
 #   sns.distplot(d["ts_array"], bins=50, kde=True, fit=norm, norm_hist=False)# , norm_hist=True) #array, bins=n_bins, fit=norm, norm_hist=True
-def create_hist(ax, data, data_stats, xlabel=None, color="blue", dyn_bins=False, density_flag=True):
+def create_hist(ax, data, data_stats, xlabel=None, color="blue"):
     bins_centres = data['bins_centres']
     bins_width   = data['bins_width']
-    fit_params_norm = data['fit_coeff_norm']
-    fitted_hist_norm = gauss(bins_centres, *fit_params_norm)
-    counts_hist_norm = data['counts_norm']
+    fit_params  = data['fit_coeff']
+    fitted_hist = gauss(bins_centres, *fit_params)
+    counts_hist = data['counts']
 
-    if density_flag:
-        ax.bar(bins_centres, height=counts_hist_norm, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
-        ax.plot(bins_centres, stats.norm.pdf(bins_centres, data_stats["mean"], data_stats["stdev"]), color="orange", linestyle="--", alpha=0.9, label='stats.norm\nμ:{0:.2e}\nσ:{1:.2e}'.format(data_stats['mean'], data_stats['stdev']))
-        ax.plot(bins_centres, fitted_hist_norm, label='curve fit\nμ:{0:.2e}\nσ:{1:.2e}'.format(fit_params_norm[1], fit_params_norm[2]), linestyle="-.", color="green")
-        ax.legend(loc='best')
-    else:
-        # ax.bar(bins_centres, height=counts_hist, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
-        counts, bins, patches = ax.hist(data_arr, bins=n_bins, alpha=0.5, edgecolor=color, color=color)
+    ax.bar(bins_centres, height=counts_hist, width=bins_width, alpha=0.5, edgecolor=color, color=color, label='data')
 
-    ax.axvline(data_stats["mean"], color="orange", linestyle="--", alpha=0.9)
-    # ax.axvline(sts["median"], color="black",  linestyle="-.", alpha=0.5)
+    # ax.plot(bins_centres, stats.norm.pdf(bins_centres, data_stats["mean"], data_stats["stdev"]), color="orange", linestyle="--", alpha=0.9, label='stats.norm\nμ:{0:.2e}\nσ:{1:.2e}'.format(data_stats['mean'], data_stats['stdev']))
 
-    if xlabel is not None:
-        ax.set_xlabel('{} (bins: {})'.format(xlabel, data['n_bins']))
+    # gauss fit
+    ax.plot(bins_centres, fitted_hist, linestyle="-.", color="green")
 
-def plot_data_summary(data):
-    rows_num=3
+    ax.legend( [ Patch(facecolor=color, edgecolor=color),
+                 Line2D([0],[0], color="green", linestyle="-.") ],
+               [ 'Data. bins:{0}\nμ:{1:.2e}\nσ:{2:.2e}'.format(data['n_bins'], data_stats['mean'], data_stats['stdev']),
+                 'Curve fit\nμ:{0:.2e}\nσ:{1:.2e}'.format(fit_params[1], abs(fit_params[2])) ],
+               loc='best' )
+
+    ax.set_xlabel('{0}'.format(xlabel))
+    ax.set_ylabel('counts')
+
+def plot_data_summary(data, save_img=False):
+    rows_num=1
+    cols_num=3
+    img_format = 'png'
     for d in data:
         # figsize in inch (width, height)
-        fig, ax = plt.subplots(nrows=rows_num, ncols=2, figsize=(9, rows_num*3))
-        fig.suptitle(d["name"]+" "+str(d["tmax"]), va="top", ha="center")
+        fig, ax = plt.subplots(nrows=rows_num, ncols=cols_num, figsize=(cols_num*4.5, rows_num*4.5))
+        fig.suptitle('{} t_{} sec'.format(d['name'], str(d['tmax'])), va='top', ha='center')
 
-        create_hist(ax[0][0], d['hist']['ts'], d['stats']['ts'], color="blue", xlabel="TS")
-        create_hist(ax[1][0], d['hist']['li_ma'], d['stats']['li_ma'], color="cyan", xlabel="Li&Ma")
-        create_hist(ax[2][0], d['hist']['flux'], d['stats']['flux'], color="green", xlabel="Flux [ph/cm²/s]" )
-        create_hist(ax[0][1], d['hist']['N_on'], d['stats']['N_on'], color="magenta", xlabel="N on", dyn_bins=True)
-        create_hist(ax[1][1], d['hist']['N_off'], d['stats']['N_off'], color="red", xlabel="N off", dyn_bins=True)
-        create_hist(ax[2][1], d['hist']['N_s'], d['stats']['N_s'], color="yellow", xlabel="N signal", dyn_bins=True)
+        create_hist(ax[0], d['hist']['ts'],    d['stats']['ts'],    color='blue',  xlabel='Model TS')
+        create_hist(ax[1], d['hist']['flux'],  d['stats']['flux'],  color='cyan',  xlabel='flux [ph/cm²/s]')
+        create_hist(ax[2], d['hist']['eflux'], d['stats']['eflux'], color='green', xlabel='eflux [erg/cm²/s]')
 
         # Important: first tight_layout(), after adjust for the title
         fig.tight_layout()
-        fig.subplots_adjust(top=0.95)
-        # plt.show()
-        plt.savefig("{0}_{1:04d}.png".format(d["name"], d["tmax"]), format="png")
-        plt.close()
-        print("saving {0}_{1:04d}.png".format(d["name"], d["tmax"]), file=sys.stderr)
+        fig.subplots_adjust(top=0.90)
+        if save_img:
+            img_filename = "{0}_{1:04d}.{2}".format(d["name"], d["tmax"], img_format)
+            plt.savefig(img_filename, format=img_format)
+            plt.close()
+            logging.debug("saving {}".format(img_filename))
+        else:
+            plt.show()
+            plt.close()
     return None
 
 if __name__ == '__main__':
@@ -272,17 +278,12 @@ if __name__ == '__main__':
     print("File read: {}".format(file_count), file=sys.stderr)
     print("     data: {}".format(len(original_data)), file=sys.stderr)
 
-    # { 'name': 'dec_0.5', 'seed': '0', 'tmax': '1800',
-    #   'ts': '8114.572', 'on_count': '6997.0', 'off_count': '5268.0',
-    #   'alpha': '0.25001633167266846', 'li_ma': '90.08091872622624',
-    #   'flux': '3.0695648428928983e-09', 'eflux': '4.839076212803621e-10' }
     ds = data_summary(original_data)
-    if not data_summary_is_ok(ds):
+    if not data_summary_is_ok(ds, pointings=5, time_slots=8, different_seeds=5000):
         exit(1)
 
     # inplace augment
     data_augmentation(ds, bins_number=50)
-
     print_data_summary(ds.values())
-    plot_data_summary(list(ds.values()))
+    plot_data_summary(list(ds.values()), save_img=True)
 
