e64ab2f858ba83bb5ed76565d6aa419b2bc8c7f9
Simone <simotrone@gmail.com>
2019-12-10 16:14:24 +0100

    weighted aeff value method

--- lib/irf_aeff.py
+++ lib/irf_aeff.py
@@ -2,6 +2,8 @@ from astropy.io import fits
 import numpy as np
 from scipy import interpolate
 from lib import utils 
+from astropy.coordinates import SkyCoord
+import math
 
 class IRF:
     def __init__(self, filename):
@@ -101,3 +103,96 @@ class EffectiveArea:
         energy_fn  = interpolate.interp2d(x = energy_mid, y = theta_mid, z = aeff_matrix)
         return energy_fn(np.log10(input_energy), input_offset)[0]
 
+    # this method and the static ones could to be outside this class
+    def weighted_value_for_region(self, region, pointing, energy, pixel_size=0.05):
+        """
+        return effective area value [m²] for a specific region
+
+        Parameters
+          region:   { 'ra': ..., 'dec': ..., 'rad': ... }
+          pointing: { 'ra': ..., 'dec': ... }
+          energy: a value in TeV (ex: 0.025, 1.0, 150.0)
+          pixel_size: a value in degree (default: 0.05)
+        """
+        # create a grid of points
+        points = self.create_pixel_map(region, pixel_size)
+        # select the points inside the region
+        internal_points = self.select_points_in_region(points, region)
+        # calculate the offsets
+        offsets = self.get_thetas(pointing, internal_points)
+
+        n_points = len(offsets)
+        val = 0
+        for t in offsets:
+            val += self.get_aeff_2d_log(t, energy) / n_points
+        return val # m2
+
+    # helpers
+    @staticmethod
+    def create_pixel_map(region, pixel_side):
+        for k in ['ra', 'dec', 'rad']:
+            if k in region:
+                continue
+            raise Exception('region data missing {} mandatory key.'.format(k))
+        if region['rad'] <= 0:
+            raise Exception('region radius must be > 0')
+        if pixel_side <= 0:
+            raise Exception('pixel side must be > 0')
+
+        region_center = { 'ra': float(region['ra']), 'dec': float(region['dec']) }
+        region_rad = utils.get_angle(float(region['rad']))
+        pixel_side_angle = utils.get_angle(float(pixel_side))
+
+        # +10% to get a bit of margin
+        n_pixel_on_rad = 1.1* region_rad / pixel_side_angle
+        if n_pixel_on_rad <= 1:
+            n_pixel_on_rad = 1
+
+        n_pixel_on_axis = float(math.ceil(n_pixel_on_rad))
+        multipliers = np.arange(-1*n_pixel_on_axis, n_pixel_on_axis+1)
+        pixels_midpoint = []
+        for i in multipliers:
+            for j in multipliers:
+                pixels_midpoint.append({ 'ra':  region_center['ra']  + i * pixel_side_angle.deg,
+                                         'dec': region_center['dec'] + j * pixel_side_angle.deg })
+        return pixels_midpoint
+
+    @staticmethod
+    def select_points_in_region(midpoints, region):
+        for k in ['ra', 'dec', 'rad']:
+            if k in region:
+                continue
+            raise Exception('region data missing {} mandatory key.'.format(k))
+        if region['rad'] <= 0:
+            raise Exception('region radius must be > 0')
+        if len(midpoints) < 1:
+            raise Exception('need at least 1 point to check')
+
+        region_center = utils.get_skycoord(region)
+        region_radius = utils.get_angle(region['rad'])
+        midpoints_ra = []
+        midpoints_dec = []
+        for p in midpoints:
+            midpoints_ra.append(p['ra'])
+            midpoints_dec.append(p['dec'])
+        midpoints_coords = SkyCoord(midpoints_ra, midpoints_dec, unit='deg', frame='icrs')
+        distances = region_center.separation(midpoints_coords)
+        return np.extract(distances < region_radius, midpoints)
+
+    @staticmethod
+    def get_thetas(pointing, midpoints):
+        for k in ['ra', 'dec']:
+            if k in pointing:
+                continue
+            raise Exception('pointing coord {} is missing.'.format(k))
+        if len(midpoints) < 1:
+            raise Exception('need at least 1 point to check')
+        pnt = utils.get_skycoord(pointing)
+        midpoints_ra = []
+        midpoints_dec = []
+        for p in midpoints:
+            midpoints_ra.append(p['ra'])
+            midpoints_dec.append(p['dec'])
+        midpoints_coords = SkyCoord(midpoints_ra, midpoints_dec, unit='deg', frame='icrs')
+        return [ ang.degree for ang in pnt.separation(midpoints_coords) ]
+    
