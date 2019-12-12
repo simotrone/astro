d60ffbd60c9f5888ab518b1cb906b8bdebfd85f7
Simone <simotrone@gmail.com>
2019-12-12 14:48:10 +0100

    different aeff eval function (with energies interval + powerlaw)

--- lib/irf_aeff.py
+++ lib/irf_aeff.py
@@ -1,6 +1,6 @@
 from astropy.io import fits
 import numpy as np
-from scipy import interpolate
+from scipy import interpolate, integrate
 from lib import utils 
 from astropy.coordinates import SkyCoord
 import math
@@ -103,10 +103,85 @@ class EffectiveArea:
         energy_fn  = interpolate.interp2d(x = energy_mid, y = theta_mid, z = aeff_matrix)
         return energy_fn(np.log10(input_energy), input_offset)[0]
 
-    # this method and the static ones could to be outside this class
-    def weighted_value_for_region(self, region, pointing, energy, pixel_size=0.05):
+    # this method use an energy range to evaluate the aeff.
+    # The energy range is binned and weighted with a powerlaw with index = e_index.
+    # Each pixel has a radial weigth and a specific column of energies weight.
+    # Lower energies have more weigth than higher.
+    # if energy range is small, the effect is trascurable - similar to weighted_value_for_region_single_energy method.
+    def weighted_value_for_region(self, region, pointing, input_energies, pixel_size=0.05, e_index=-2.4):
+        """return effective area value [m²] for a specific region
+
+        Parameters
+          region:   { 'ra': ..., 'dec': ..., 'rad': ... }
+          pointing: { 'ra': ..., 'dec': ... }
+          energies: a couple of values in TeV (ex: [ 0.025, 1.0 ])
+          pixel_size: a value in degree (default: 0.05)
+          e_index: is the powerlaw index (default: -2.4)
         """
-        return effective area value [m²] for a specific region
+        if len(input_energies) != 2:
+            raise Exception('need two energies')
+
+        # create a grid of points
+        points = self.create_pixel_map(region, pixel_size)
+        # select the points inside the region
+        internal_points = self.select_points_in_region(points, region)
+        # calculate the offsets
+        offsets = self.get_thetas(pointing, internal_points)
+
+        log_energies = np.log10(input_energies)
+        # N steps for every unit of log energy
+        steps = int(np.ceil(log_energies[1]-log_energies[0]) * 10)
+        energies = 10**np.linspace(log_energies[0], log_energies[1], steps)
+        powerlaw = lambda x: x**e_index
+        i_full = integrate.quad(powerlaw, input_energies[0], input_energies[1])
+        i_partials = [ integrate.quad(powerlaw, energies[i], energies[i+1]) for i,v in enumerate(energies[:-1]) ]
+        i_factor = [ p[0]/i_full[0] for p in i_partials ]
+        energies_middle = (energies[1:]+energies[:-1])/2
+        n_points = len(offsets)
+        val = 0
+        for t in offsets:
+            for i, en in enumerate(energies_middle):
+                val += self.get_aeff_2d_log(t, en) * i_factor[i] / n_points
+        return val
+
+    # this method use an energy range to evaluate the aeff. The energy range is
+    # binned and every matrix cube (pixel distance * energy bin) have the same
+    # weight.
+    def weighted_value_for_region_v02(self, region, pointing, input_energies, pixel_size=0.05):
+        """return effective area value [m²] for a specific region
+
+        Parameters
+          region:   { 'ra': ..., 'dec': ..., 'rad': ... }
+          pointing: { 'ra': ..., 'dec': ... }
+          energies: a couple of values in TeV (ex: [ 0.025, 1.0 ])
+          pixel_size: a value in degree (default: 0.05)
+        """
+        if len(input_energies) != 2:
+            raise Exception('need two energies')
+
+        # create a grid of points
+        points = self.create_pixel_map(region, pixel_size)
+        # select the points inside the region
+        internal_points = self.select_points_in_region(points, region)
+        # calculate the offsets
+        offsets = self.get_thetas(pointing, internal_points)
+
+        log_energies = np.log10(input_energies)
+        diff = np.ceil(log_energies[1]-log_energies[0])
+        steps = int(diff * 10) # N steps for every unit of log energy
+        energies = 10**np.linspace(log_energies[0], log_energies[1], steps)
+        n_points = len(offsets) * len(energies)
+        val = 0
+        for t in offsets:
+            for en in energies:
+                val += self.get_aeff_2d_log(t, en) / n_points
+        return val
+
+    # Deprecated 2019-12-11
+    # this method is old. just a plain output from an array of points and ONE
+    # energy (usually the middle point of the energy range)
+    def weighted_value_for_region_single_energy(self, region, pointing, energy, pixel_size=0.05):
+        """return effective area value [m²] for a specific region
 
         Parameters
           region:   { 'ra': ..., 'dec': ..., 'rad': ... }
