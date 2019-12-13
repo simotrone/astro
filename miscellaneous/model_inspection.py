ef4951b2afcfff3d2bd7ff0296f794add4e9095a
Simone <simotrone@gmail.com>
2019-12-13 12:24:02 +0100

    more info from model inspection (flux, eflux, params)

--- miscellaneous/model_inspection.py
+++ miscellaneous/model_inspection.py
@@ -2,8 +2,35 @@ import gammalib
 import os
 import sys
 
+# all in MeV
+def flux_eval_following_ctools(energies, prefactor, index=-2.4, epivot=0.3e6):
+    _exp = index + 1
+    res = prefactor * epivot / _exp * ( (energies[1]/epivot)**_exp - (energies[0]/epivot)**_exp )
+    return res
+
+def eflux_eval_following_ctools(energies, prefactor, index=-2.4, epivot=0.3e6):
+    _exp = index + 2
+    _epivot2 = epivot**2
+    res = prefactor * _epivot2 / _exp * ( (energies[1]/epivot)**_exp - (energies[0]/epivot)**_exp )
+    return res
+
+mev2erg = 1.6021765e-6 # MeV => erg
+
 input_model = sys.argv[1] 
 models = gammalib.GModels(input_model)
 print(models)
-print("TS:", models[0].ts())
+first_model = models[0]
+m_spect = first_model.spectral()
+print("TS:", first_model.ts())
 
+emin = gammalib.GEnergy(25, 'GeV')
+emax = gammalib.GEnergy(150, 'TeV')
+r = m_spect.flux(emin, emax)
+er = m_spect.eflux(emin, emax)
+print('Flux:', r)
+print('EFlux:', er)
+for par in ['Prefactor', 'PivotEnergy', 'Index']:
+    print(par, m_spect[par].value(), m_spect[par].error())
+print('flux: {0:.4e} ph/cm²/s'.format(flux_eval_following_ctools([25e3, 150e6], m_spect['Prefactor'].value())))
+my_eflux = eflux_eval_following_ctools([25e3, 150e6], m_spect['Prefactor'].value())
+print('eflux: {0:.4e} MeV/cm²/s => {1:.4e} erg/cm²/s'.format(my_eflux, my_eflux*mev2erg))
